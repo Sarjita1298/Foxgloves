7014b22101c68f600c284f67b5e7f31daab8cd49
sarjita1298 <sarjitachaurasiya1298@gmail.com>
2025-04-30 13:16:20 +0530

    commit

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -33,9 +33,9 @@
                 <a class="" href="contact">
                   Contact Us
                 </a>
-                <a class="" href="about">
+                {{-- <a class="" href="about">
                   Our Mission
-                </a>
+                </a> --}}
                 {{-- <a class="" href="guest-catering-canteen">
                   Guest Sevice
                 </a> --}}
